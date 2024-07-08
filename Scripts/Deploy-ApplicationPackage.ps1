dbb8cfa9fbac2bbb41e2ad27bc1dead34cb582e2
Alireza <alirezabaloochi.work2@gmail.com>
2024-07-08 14:25:22 +0330

    use destination as iisApp

--- Scripts/Deploy-ApplicationPackage.ps1
+++ Scripts/Deploy-ApplicationPackage.ps1
@@ -1,16 +1,14 @@
 $msdeploy = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe"
 
 $source = $args[0]
-$destination = $args[1]
-$recycleApp = $args[2]
-$computerName = $args[3]
-$username = $args[4]
-$password = $args[5]
-$delete = $args[6]
-$skipDirectory = $args[7]
+$recycleApp = $args[1]
+$computerName = $args[2]
+$username = $args[3]
+$password = $args[4]
+$delete = $args[5]
+$skipDirectory = $args[6]
 
 $computerNameArgument = "$computerName/MsDeploy.axd?site=$recycleApp"
-
 $contentPath = $source
 
 # Initialize the msdeploy arguments array
@@ -18,16 +16,20 @@ $contentPath = $source
     "-verb:sync",
     "-allowUntrusted",
     "-source:contentPath=$contentPath",
-    "-dest:auto,computerName=$computerNameArgument,username=$username,password=$password,AuthType=Basic"
+    "-dest:iisApp=$recycleApp,computerName=$computerNameArgument,username=$username,password=$password,AuthType=Basic"
 )
 
-if ($delete -NotMatch "true") {
+if ($delete -ne "true") {
     $msdeployArguments.Add("-enableRule:DoNotDeleteRule")
 }
 
 if ($skipDirectory) {
     $msdeployArguments.Add("-skip:Directory=$skipDirectory")
 }
+
+# Output the msdeploy command for debugging purposes
+# Write-Host "Running msdeploy with the following arguments:"
+# $msdeployArguments | ForEach-Object { Write-Host $_ }
 
 # Execute the msdeploy command with the arguments
 & $msdeploy @msdeployArguments
